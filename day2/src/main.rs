b36b26ca59b6f6f9c8809756ba4b422289bc9f06
afllama <andriiflama@gmail.com>
2023-11-29 23:59:59 +0100

    adc

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,4 +1,11 @@
-fn main() {
-    let path = include_str!(env::args().collect::Vec<String>()[1].parse::<str>());
+use std::env;
+use std::fs;
 
+fn main() {
+    let path: &str = &env::args().collect::<Vec<String>>()[2];
+    let content = fs::read_to_string(path);
+    let binding = content.expect("reason");
+    let lines: Vec<&str> = binding.split("\n\n").collect::<Vec<&str>>();
+    println!("{:?}", lines);
+    //println!("Usage: {} <file-path>", env::args().next().unwrap_or_else(|| String::from("program")));
 }
